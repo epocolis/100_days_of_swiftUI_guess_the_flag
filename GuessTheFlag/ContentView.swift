2781f7a411b803600f682df372c31574e6a2f81a
leotis <mekdemcrossit@tutanota.com>
2021-04-21 15:04:04 -0400

    colors and frames

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red:1, green:0.8, blue:0)
+            Text("Your content")
+        }
     }
 }
 
