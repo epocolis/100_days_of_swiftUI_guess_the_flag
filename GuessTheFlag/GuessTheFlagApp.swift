5f5a486a9049345ab07cb1bf9cb2bf80a4b6270e
leotis <mekdemcrossit@tutanota.com>
2021-04-21 14:40:07 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Leotis buchanan on 2021-04-21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
